d83bd1e79aaacfa0e7a9c938f8a2db483d4b309f
David Paul Eligio <dp.eligio18@gmail.com>
2021-09-18 15:23:14 +0800

    Initial Commit

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,9 +10,9 @@
         <!-- Sidebar user panel (optional) -->
         @auth
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
+            {{-- <div class="image">
                 <img src="{{ asset('AdminLTE-3.1.0/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-            </div>
+            </div> --}}
             <div class="info">
                 <a href="#" class="d-block">
                     @if (Auth::user()->student)
